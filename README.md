cf79b8c3cf39054d04f2863217ef6f8dec1d8951
Nick Meinhold <nick.meinhold@gmail.com>
2021-05-12 22:50:26 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# firebase_extension_publish_new_user_data
+
+On Firebase user creation, publish user data to a given PubSub topic.
+
+The published messages will be consumed by another service such as: [on_user_creation_microservice](https://github.com/Adventures-In/on_user_creation_microservice)
+
+## Setup 
+- edit `.firebaserc` to add the relevant project name
+- edit `index.ts` to add the relevant topic name 
+
+## Deploy 
+- run `firebase deploy --only functions`
---
+++ functions/src/index.ts
@@ -0,0 +1,30 @@
+import {PubSub} from "@google-cloud/pubsub";
+import * as functions from "firebase-functions";
+
+const topicName = "TOPIC_NAME";
+const topic = (new PubSub()).topic(topicName);
+
+/**
+ * Publishes a message with the user data to the given Pub/Sub topic.
+ */
+exports.publishNewUser = functions.auth.user().onCreate(async (user) => {
+  // Create a message with the user data
+  const messageObject = {
+    data: {
+      message: user,
+    },
+  };
+  const messageBuffer = Buffer.from(JSON.stringify(messageObject), "utf8");
+
+  functions.logger.log("Attempting to publish message:", messageBuffer);
+
+  // Publish the message
+  try {
+    await topic.publish(messageBuffer);
+    functions.logger.log("Message published.");
+    return Promise.resolve();
+  } catch (err) {
+    console.error(err);
+    return Promise.reject(err);
+  }
+});
